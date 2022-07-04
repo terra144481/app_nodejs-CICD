3daeb6d07ced8dff1c1be4bb8b445d52cc0bafe2
Ivan Lovkin <terra144481@gmail.com>
2022-07-05 00:44:42 +0300

    add aws cli instal in terraform.sh

--- Jenkins/scripts/terraform_install.sh
+++ Jenkins/scripts/terraform_install.sh
@@ -4,3 +4,7 @@ curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install terraform
 terraform -version
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+aws --version
